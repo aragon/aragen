f59eb644548c650b9c615164a109c8f0ca4efddd
perissology <perissology@protonmail.com>
2018-10-22 11:34:32 -0700

    export mnemonic

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
-	// ENS is deterministically deployed to this address
-	ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1'
+  // ENS is deterministically deployed to this address
+  ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',
+  MNEMONIC: 'explain tackle mirror kit van hammer degree position ginger unfair soup bonus'
 }
