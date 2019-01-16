b7452488b0e951ddc2d502a72f1c80088e490338
galactusss <gabrielpk.18@gmail.com>
2019-01-16 17:32:00 -0300

    update to new aragonCLI version

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   // ENS is deterministically deployed to this address
   ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',
-  ConsoleReporter: require('./src/ConsoleReporter'),
+  ConsoleReporter: require('./src/reporters/ConsoleReporter'),
   commands: {
-    devchain: require('./src/commands/devchain')
-  }
+    devchain: require('./src/commands/devchain'),
+  },
 }
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 require('@babel/polyfill')
-const ConsoleReporter = require('./ConsoleReporter')
+const ConsoleReporter = require('./reporters/ConsoleReporter')
 
 // Set up commands
-const cmd = require('yargs')
-  .commandDir('./commands')
+const cmd = require('yargs').commandDir('./commands')
 
 cmd.alias('h', 'help')
 cmd.alias('v', 'version')
@@ -12,13 +11,40 @@ cmd.alias('v', 'version')
 // Configure CLI behavior
 cmd.demandCommand(1, 'You need to specify a command')
 
+// Set global options
+cmd.option('silent', {
+  description: 'Silence output to terminal',
+  default: false,
+})
+
+cmd.option('debug', {
+  description: 'Show more output to terminal',
+  default: false,
+  coerce: debug => {
+    if (debug || process.env.DEBUG) {
+      global.DEBUG_MODE = true
+      return true
+    }
+  },
+})
+
+// APM
+cmd.option('apm.ens-registry', {
+  description:
+    "Address of the ENS registry. This will be overwritten if the selected '--environment' from your arapp.json includes a `registry` property",
+  default: require('../index').ens,
+})
+cmd.group(['apm.ens-registry'], 'APM:')
+
 // Run
 const reporter = new ConsoleReporter()
 // reporter.debug(JSON.stringify(process.argv))
-cmd.fail((msg, err, yargs) => {
-  if (!err) yargs.showHelp()
-  reporter.error(msg || err.message || 'An error occurred')
-  reporter.debug(err && err.stack)
-}).parse(process.argv.slice(2), {
-  reporter
-})
+cmd
+  .fail((msg, err, yargs) => {
+    if (!err) yargs.showHelp()
+    reporter.error(msg || err.message || 'An error occurred')
+    reporter.debug(err && err.stack)
+  })
+  .parse(process.argv.slice(2), {
+    reporter,
+  })
