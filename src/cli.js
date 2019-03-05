cb6a56fc1b5984e4cd856ced18f6e1b2a23a1361
RJ Ewing <rj@rjewing.com>
2019-03-04 16:25:19 -0800

    add basic cli & devchain cmd (#24)
    * add basic cli & devchain cmd
    
    * add verbose mode to devchain cmd
    
    * Delete package-lock.json
    
    * Bump ganache-core version
    
    Discussed in aragon-cli/issues/321
    
    * Bump yargs to v12
    
    * update to new aragonCLI version
    
    * small fix on the bash script
    
    * Ping ganache-core v2.2.1
    
    * fix bash script & remote apm.options
    
    * chore: change version to beta

---
+++ src/cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+require('@babel/polyfill')
+const ConsoleReporter = require('./reporters/ConsoleReporter')
+
+// Set up commands
+const cmd = require('yargs').commandDir('./commands')
+
+cmd.alias('h', 'help')
+cmd.alias('v', 'version')
+
+// Configure CLI behavior
+cmd.demandCommand(1, 'You need to specify a command')
+
+// Set global options
+cmd.option('silent', {
+  description: 'Silence output to terminal',
+  default: false,
+})
+
+cmd.option('debug', {
+  description: 'Show more output to terminal',
+  default: false,
+  coerce: debug => {
+    if (debug || process.env.DEBUG) {
+      global.DEBUG_MODE = true
+      return true
+    }
+  },
+})
+
+// Run
+const reporter = new ConsoleReporter()
+// reporter.debug(JSON.stringify(process.argv))
+cmd
+  .fail((msg, err, yargs) => {
+    if (!err) yargs.showHelp()
+    reporter.error(msg || err.message || 'An error occurred')
+    reporter.debug(err && err.stack)
+  })
+  .parse(process.argv.slice(2), {
+    reporter,
+  })
