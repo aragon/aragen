eb92d4cbaa8eaefe0b5b08a51b78ab865c616842
0xGabi <gabrielpk.18@gmail.com>
2019-07-29 01:15:29 -0300

    Export status command

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
   ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',
   commands: {
     devchain: require('./src/commands/devchain'),
+    status: require('./src/commands/status'),
   },
 }
