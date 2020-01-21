e995782aa94898b513801c4ee48a3a3f77bef477
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-01-21 02:34:39 -0300

    v5.4.3 (#96)

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ module.exports = {
     start: require('./src/commands/start'),
     status: require('./src/commands/status'),
   },
+  daoFacotry: '0x5d94e3e7aec542ab0f9129b9a7badeb5b3ca0f77',
+  minimeFactory: '0xd526b7aba39cccf76422835e7fd5327b98ad73c9',
+  fifsResolvingRegistrar: '0xf1f8aac64036cdd399886b1c157b7e3b361093f3',
+  apmRegistry: '0x32296d9f8fed89658668875dc73cacf87e8888b2',
+  openRegistry: '0x983f1d68a781abbc3a49676a6399dd173de25105',
+  hatchRegistry: '0xd4592f8b2116d00fc4ce183773eb24c584060cfc',
 }
